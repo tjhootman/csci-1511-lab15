f86e61e62ccfc1b40e208371b0b11ecde088b337
tjhootman <tjhootman@gmail.com>
2025-04-25 19:16:29 -0400

    Add style, title, and axis labels

--- cubes_plot_1.py
+++ cubes_plot_1.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 
+# create a list of numbers for the x-axis
 x_values = list(range(1, 6))
+# create empty list to store cubic values
 y_values = []
 
+# calculate the cube for each number in x_values and store it in y_values
 for num in x_values:
     num **= 3
     y_values.append(num)
 
-fig, ax = plt.subplots()
-ax.plot(x_values,y_values,linewidth=2)
+# apply plot style
+plt.style.use('seaborn-v0_8-darkgrid')
 
+# plot the line
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(x_values, y_values, color='mediumseagreen', linewidth=2)
+
+# add the title and axes labels
+plt.title('First 5 Cubic Numbers', fontsize=16)
+plt.xlabel('Number', fontsize=12)
+plt.ylabel('Cube', fontsize=12)
+plt.tick_params(axis='both', labelsize=10)
+
+# show the plot
 plt.show()
--- cubes_plot_2.py
+++ cubes_plot_2.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 
+# create a list of numbers for the x-axis
 x_values = list(range(1, 5001))
+# create empty list to store cubic values
 y_values = []
 
+# calculate the cube for each number in x_values and store it in y_values
 for num in x_values:
     num **= 3
     y_values.append(num)
 
-fig, ax = plt.subplots()
-ax.scatter(x_values,y_values, c=y_values, cmap=plt.cm.cool, s=10)
+# apply plot style
+plt.style.use('dark_background')
 
+# plot the line
+fig, ax = plt.subplots(figsize=(10, 8))
+ax.scatter(x_values,y_values, c=y_values, cmap=plt.cm.plasma, s=10)
+
+# add the title and axes labels
+plt.title('First 5000 Cubic Numbers', fontsize=16)
+plt.xlabel('Number', fontsize=12)
+plt.ylabel('Cube', fontsize=12)
+plt.tick_params(axis='both', labelsize=10)
+
+# show the plot
 plt.show()
